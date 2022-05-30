d853c9cd4a5d10d423fbc13afdc11048615d9fd0
Aleksei Ovchinnikov <eastonsc6@yandex.ru>
2022-05-30 13:49:16 +0300

    fix report data

--- src/pages/ReportML/index.js
+++ src/pages/ReportML/index.js
@@ -36,160 +36,168 @@ const ReportML = () => {
           <p className='middle-title-cell'>Итоговая прибыль от проведения рассылки, руб</p>
           <p className='middle-title-cell'>Выручка, руб</p>
           <p className='middle-title-cell'>Доля рассылки в выручке , %</p>
+
           <p className='cell-start-group'>Молочная продукция - случайный лес</p>
-          <p className='bg-dark'>1 000</p>
-          <p className='bg-light'>100</p>
-          <p className='bg-dark'>543</p>
-          <p className='bg-light'>544</p>
-          <p className='bg-dark'>435 628 ₽</p>
-          <p className='bg-light'>54,40%</p>
-          <p className='bg-dark'>801 ₽</p>
-          <p className='bg-light'>11</p>
-          <p className='bg-dark'>12</p>
-          <p className='bg-light'>32 700 ₽</p>
-          <p className='bg-dark'>12,00%</p>
-          <p className='bg-light'>2 725 ₽</p>
-          <p className='bg-dark'>10,75</p>
-          <p className='bg-light'>327 000 ₽</p>
-          <p className='bg-dark'>108 628 ₽</p>
-          <p className='bg-light'>33%</p>
-          <p className='bg-dark'>1 451 180,00</p>
-          <p className='bg-light'>40%</p>
-          <p className='bg-dark'>-1 407 729 ₽</p>
-          <p className='bg-light'>1 000 000</p>
-          <p className='bg-dark'>10,9%</p>
-          <p className='cell-start-group'>Мясная продукция - случайный лес</p>
-          <p className='bg-light'>1 000</p>
-          <p>100</p>
-          <p className='bg-light'>645</p>
-          <p>645</p>
-          <p className='bg-light'>562 314 ₽</p>
-          <p>64,50%</p>
-          <p className='bg-light'>872 ₽</p>
-          <p>5</p>
-          <p className='bg-light'>6</p>
-          <p>17 630 ₽</p>
-          <p className='bg-light'>6,00%</p>
-          <p>2 938 ₽</p>
-          <p className='bg-light'>4,16</p>
-          <p>176 300 ₽</p>
-          <p className='bg-light'>386 014 ₽</p>
-          <p>219%</p>
-          <p className='bg-light'>887 646,00</p>
-          <p>40%</p>
-          <p className='bg-light'>-733 240 ₽</p>
-          <p>1 000 000</p>
-          <p className='bg-light'>38,6%</p>
-          <p className='cell-start-group'>Акция 1+1 - дерево решений</p>
-          <p className='bg-dark'>1 000</p>
-          <p className='bg-light'>100</p>
-          <p className='bg-dark'>342</p>
-          <p className='bg-light'>345</p>
-          <p className='bg-dark'>321 456 ₽</p>
-          <p className='bg-light'>34,50%</p>
-          <p className='bg-dark'>932 ₽</p>
-          <p className='bg-light'>19</p>
-          <p className='bg-dark'>19</p>
-          <p className='bg-light'>43 222 ₽</p>
-          <p className='bg-dark'>19,00%</p>
-          <p className='bg-light'>2 275 ₽</p>
-          <p className='bg-dark'>5,65</p>
-          <p className='bg-light'>432 220 ₽</p>
-          <p className='bg-dark'>-110 764 ₽</p>
-          <p className='bg-light'>-26%</p>
-          <p className='bg-dark'>2 537 871,00</p>
-          <p className='bg-light'>40%</p>
-          <p className='bg-dark'>-2 582 177 ₽</p>
-          <p className='bg-light'>1 000 000</p>
-          <p className='bg-dark'>-11,1%</p>
-          <p className='cell-start-group'>Выдача карт - ml 1</p>
-          <p className='bg-light'>1 000</p>
-          <p>100</p>
-          <p className='bg-light'>872</p>
-          <p>873</p>
-          <p className='bg-light'>871 324 ₽</p>
-          <p>87,30%</p>
-          <p className='bg-light'>998 ₽</p>
-          <p>21</p>
-          <p className='bg-light'>21</p>
-          <p>67 431 ₽</p>
-          <p className='bg-light'>21,00%</p>
-          <p>3 211 ₽</p>
-          <p className='bg-light'>1,82</p>
-          <p>674 310 ₽</p>
-          <p className='bg-light'>197 014 ₽</p>
-          <p>29%</p>
-          <p className='bg-light'>1 333 332,00</p>
-          <p>40%</p>
-          <p className='bg-light'>-1 254 526 ₽</p>
-          <p>1 000 000</p>
-          <p className='bg-light'>19,7%</p>
-          <p className='cell-start-group'>Масла категория - случайный лес</p>
-          <p className='bg-dark'>1 000</p>
-          <p className='bg-light'>100</p>
-          <p className='bg-dark'>721</p>
-          <p className='bg-light'>722</p>
-          <p className='bg-dark'>782 988 ₽</p>
-          <p className='bg-light'>72,20%</p>
-          <p className='bg-dark'>1 084 ₽</p>
+          <p className='bg-dark'>2 000</p>
+          <p className='bg-light'>200</p>
+          <p className='bg-dark'>1358</p>
+          <p className='bg-light'>1 360</p>
+          <p className='bg-dark'>1 089 070 ₽</p>
+          <p className='bg-light'>68,00%</p>
+          <p className='bg-dark'>1 762 ₽</p>
           <p className='bg-light'>32</p>
-          <p className='bg-dark'>32</p>
-          <p className='bg-light'>103 287 ₽</p>
-          <p className='bg-dark'>32,00%</p>
-          <p className='bg-light'>3 228 ₽</p>
-          <p className='bg-dark'>3,71</p>
-          <p className='bg-light'>1 032 870 ₽</p>
-          <p className='bg-dark'>-249 882 ₽</p>
-          <p className='bg-light'>-24%</p>
-          <p className='bg-dark'>2 299 981,00</p>
-          <p className='bg-light'>40%</p>
-          <p className='bg-dark'>-2 399 934 ₽</p>
+          <p className='bg-dark'>43</p>
+          <p className='bg-light'>58 137 ₽</p>
+          <p className='bg-dark'>21,50%</p>
+          <p className='bg-light'>1 352 ₽</p>
+          <p className='bg-dark'>2,99</p>
+          <p className='bg-light'>581 371 ₽</p>
+          <p className='bg-dark'>507 699 ₽</p>
+          <p className='bg-light'>87%</p>
+          <p className='bg-dark'>0,00</p>
+          <p className='bg-light'>30%</p>
+          <p className='bg-dark'>152 310 ₽</p>
           <p className='bg-light'>1 000 000</p>
-          <p className='bg-dark'>-25,0%</p>
-          <p className='cell-start-group'>Молочная продукция - градиентный бустинг</p>
-          <p className='bg-light'>1 000</p>
-          <p>100</p>
-          <p className='bg-light'>621</p>
-          <p>621</p>
-          <p className='bg-light'>563 986 ₽</p>
-          <p>62,10%</p>
-          <p className='bg-light'>908 ₽</p>
-          <p>10</p>
-          <p className='bg-light'>11</p>
-          <p>28 654 ₽</p>
-          <p className='bg-light'>11,00%</p>
-          <p>2 605 ₽</p>
-          <p className='bg-light'>2,26</p>
-          <p>286 540 ₽</p>
-          <p className='bg-light'>277 446 ₽</p>
-          <p>97%</p>
-          <p className='bg-light'>1 694 653,00</p>
-          <p>40%</p>
-          <p className='bg-light'>-1 583 675 ₽</p>
+          <p className='bg-dark'>50,8%</p>
+
+          <p className='cell-start-group'>Мясная продукция - случайный лес</p>
+          <p className='bg-light'>2 000</p>
+          <p>200</p>
+          <p className='bg-light'>1613</p>
+          <p>1 613</p>
+          <p className='bg-light'>1 405 785 ₽</p>
+          <p>80,63%</p>
+          <p className='bg-light'>1 918 ₽</p>
+          <p>5</p>
+          <p className='bg-light'>54</p>
+          <p>63 293 ₽</p>
+          <p className='bg-light'>27,00%</p>
+          <p>1 172 ₽</p>
+          <p className='bg-light'>10,39</p>
+          <p>632 930 ₽</p>
+          <p className='bg-light'>772 855 ₽</p>
+          <p>122%</p>
+          <p className='bg-light'>0,00</p>
+          <p>30%</p>
+          <p className='bg-light'>231 856 ₽</p>
           <p>1 000 000</p>
-          <p className='bg-light'>27,7%</p>
+          <p className='bg-light'>77,3%</p>
+
+          <p className='cell-start-group'>Акция 1+1 - дерево решений</p>
+          <p className='bg-dark'>2 000</p>
+          <p className='bg-light'>200</p>
+          <p className='bg-dark'>2180</p>
+          <p className='bg-light'>2 183</p>
+          <p className='bg-dark'>2 178 310 ₽</p>
+          <p className='bg-light'>109,13%</p>
+          <p className='bg-dark'>2 196 ₽</p>
+          <p className='bg-light'>3</p>
+          <p className='bg-dark'>21</p>
+          <p className='bg-light'>72 461 ₽</p>
+          <p className='bg-dark'>10,50%</p>
+          <p className='bg-light'>3 451 ₽</p>
+          <p className='bg-dark'>2,61</p>
+          <p className='bg-light'>724 606 ₽</p>
+          <p className='bg-dark'>1 453 704 ₽</p>
+          <p className='bg-light'>201%</p>
+          <p className='bg-dark'>0,00</p>
+          <p className='bg-light'>30%</p>
+          <p className='bg-dark'>436 111 ₽</p>
+          <p className='bg-light'>1 000 000</p>
+          <p className='bg-dark'>145,4%</p>
+
+          <p className='cell-start-group'>Выдача карт - ml 1</p>
+          <p className='bg-light'>2 000</p>
+          <p>200</p>
+          <p className='bg-light'>855</p>
+          <p>863</p>
+          <p className='bg-light'>803 640 ₽</p>
+          <p>43,13%</p>
+          <p className='bg-light'>2 050 ₽</p>
+          <p>23</p>
+          <p className='bg-light'>33</p>
+          <p>67 646 ₽</p>
+          <p className='bg-light'>16,50%</p>
+          <p>2 050 ₽</p>
+          <p className='bg-light'>12,94</p>
+          <p>676 455 ₽</p>
+          <p className='bg-light'>127 185 ₽</p>
+          <p>19%</p>
+          <p className='bg-light'>0,00</p>
+          <p>30%</p>
+          <p className='bg-light'>38 155 ₽</p>
+          <p>1 000 000</p>
+          <p className='bg-light'>12,7%</p>
+
+          <p className='cell-start-group'>Масла категория - случайный лес</p>
+          <p className='bg-dark'>2 000</p>
+          <p className='bg-light'>200</p>
+          <p className='bg-dark'>1553</p>
+          <p className='bg-light'>1 553</p>
+          <p className='bg-dark'>1 409 965 ₽</p>
+          <p className='bg-light'>77,63%</p>
+          <p className='bg-dark'>1 998 ₽</p>
+          <p className='bg-light'>21</p>
+          <p className='bg-dark'>12</p>
+          <p className='bg-light'>65 935 ₽</p>
+          <p className='bg-dark'>6,00%</p>
+          <p className='bg-light'>5 495 ₽</p>
+          <p className='bg-dark'>15,04</p>
+          <p className='bg-light'>659 346 ₽</p>
+          <p className='bg-dark'>750 619 ₽</p>
+          <p className='bg-light'>114%</p>
+          <p className='bg-dark'>0,00</p>
+          <p className='bg-light'>30%</p>
+          <p className='bg-dark'>225 186 ₽</p>
+          <p className='bg-light'>1 000 000</p>
+          <p className='bg-dark'>75,1%</p>
+
+          <p className='cell-start-group'>Молочная продукция - градиентный бустинг</p>
+          <p className='bg-light'>2 000</p>
+          <p>200</p>
+          <p className='bg-light'>1803</p>
+          <p>1 805</p>
+          <p className='bg-light'>1 957 470 ₽</p>
+          <p>90,25%</p>
+          <p className='bg-light'>2 386 ₽</p>
+          <p>32</p>
+          <p className='bg-light'>12</p>
+          <p>78 733 ₽</p>
+          <p className='bg-light'>6,00%</p>
+          <p>6 561 ₽</p>
+          <p className='bg-light'>5,36</p>
+          <p>787 326 ₽</p>
+          <p className='bg-light'>1 170 144 ₽</p>
+          <p>149%</p>
+          <p className='bg-light'>0,00</p>
+          <p>30%</p>
+          <p className='bg-light'>351 043 ₽</p>
+          <p>1 000 000</p>
+          <p className='bg-light'>117,0%</p>
+
+
           <p className='bottom-summary-cell bottom-summary-left-cell'>Итого</p>
-          <p className='bottom-summary-cell bg-dark'>6 000</p>
-          <p className='bottom-summary-cell bg-light'>600</p>
-          <p className='bottom-summary-cell bg-dark'>3 744</p>
-          <p className='bottom-summary-cell bg-light'>3 750</p>
-          <p className='bottom-summary-cell bg-dark'>3 537 696 ₽</p>
-          <p className='bottom-summary-cell bg-light'>62,50%</p>
+          <p className='bottom-summary-cell bg-dark'>12 000</p>
+          <p className='bottom-summary-cell bg-light'>1 200</p>
+          <p className='bottom-summary-cell bg-dark'>9 360</p>
+          <p className='bottom-summary-cell bg-light'>9 375</p>
+          <p className='bottom-summary-cell bg-dark'>8 844 240 ₽</p>
+          <p className='bottom-summary-cell bg-light'>78,13%</p>
           <p className='bottom-summary-cell bg-dark'>943 ₽</p>
-          <p className='bottom-summary-cell bg-light'>98</p>
-          <p className='bottom-summary-cell bg-dark'>101</p>
-          <p className='bottom-summary-cell bg-light'>292 924 ₽</p>
-          <p className='bottom-summary-cell bg-dark'>16,83%</p>
-          <p className='bottom-summary-cell bg-light'>2 900 ₽</p>
-          <p className='bottom-summary-cell bg-dark'>3,71</p>
-          <p className='bottom-summary-cell bg-light'>2 929 240 ₽</p>
-          <p className='bottom-summary-cell bg-dark'>608 456 ₽</p>
-          <p className='bottom-summary-cell bg-light'>21%</p>
-          <p className='bottom-summary-cell bg-dark'>10 204 663 ₽</p>
-          <p className='bottom-summary-cell bg-light'>40%</p>
-          <p className='bottom-summary-cell bg-dark'>-9 961 281 ₽</p>
+          <p className='bottom-summary-cell bg-light'>116</p>
+          <p className='bottom-summary-cell bg-dark'>175</p>
+          <p className='bottom-summary-cell bg-light'>406 203 ₽₽</p>
+          <p className='bottom-summary-cell bg-dark'>14,58%</p>
+          <p className='bottom-summary-cell bg-light'>2 321 ₽</p>
+          <p className='bottom-summary-cell bg-dark'>5,36</p>
+          <p className='bottom-summary-cell bg-light'>4 062 035 ₽</p>
+          <p className='bottom-summary-cell bg-dark'>4 782 205 ₽</p>
+          <p className='bottom-summary-cell bg-light'>118%</p>
+          <p className='bottom-summary-cell bg-dark'>- ₽</p>
+          <p className='bottom-summary-cell bg-light'>30%</p>
+          <p className='bottom-summary-cell bg-dark'>1 434 662 ₽</p>
           <p className='bottom-summary-cell bg-light'>6 000 000</p>
-          <p className='bottom-summary-cell bg-dark'>10,1%</p>
+          <p className='bottom-summary-cell bg-dark'>79,7%</p>
         </div>
       </div>
     </div>
